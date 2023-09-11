4a45c86206f6bab479e49c0dcebdff36eea9b7c7
Sara <sara.vlasic@fpmoz.sum.ba>
2023-09-11 18:59:08 +0200

    Neke promjene

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,16 @@
   <v-container>
         <v-row>
           <v-col
-            v-for="n in 24"
-            :key="n"
+            v-for="food in foods"
+            :key="food.title"
             cols="4"
           >
-            <v-card height="200"></v-card>
+            <v-card height="200">
+              <div class="food-image">
+                <img :src="food.image" alt="Food" />
+              </div>
+              {{food.title}}
+            </v-card>
           </v-col>
         </v-row>
       </v-container>
@@ -14,12 +19,53 @@
 
 <script>
   //import HelloWorld from '../components/HelloWorld'
+  import axios from 'axios'
 
   export default {
     name: 'Home',
+
+    data() {
+      return{
+        foods: []
+      }
+    },
+
+    created() {
+      this.getData();
+    },
+
+    methods: {
+      getData(){
+        let api = "https://the-cocktail-db3.p.rapidapi.com/"
+        this.axios.get(api, {
+          headers: {
+            'X-RapidAPI-Key': '1f8770d103mshf8d79491f0ae610p1b6a37jsna005cb0020cc',
+            'X-RapidAPI-Host': 'the-cocktail-db3.p.rapidapi.com'
+          }
+        }).then((response) => {
+          console.log(response.data)
+          this.foods = response.data;
+        })
+      }
+    }
 
     //components: {
     //  HelloWorld,
     //},
   }
 </script>
+
+<style scoped>
+.food-image {
+  display: flex; /* Omogućava korištenje fleksibilnog modela */
+  justify-content: center; /* Poravnava horizontalno na sredinu */
+  align-items: center; /* Poravnava vertikalno na sredinu */
+  height: 150px; /* Postavlja željenu visinu za karticu */
+}
+.food-image img {
+  max-width: 100%;
+  max-height: 130px; /* Postavlja željenu maksimalnu visinu */
+  object-fit: cover; /* Omogućava da se slika pravilno skalira */
+
+}
+</style>
